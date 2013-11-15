e6eed98be2b792ab4059839c7c9ed1390590ac74
Jesse Nelson <spheromak@gmail.com>
2013-11-14 22:20:11 -0800

    update upstream etcd use latest etcd release

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,11 +4,6 @@ include_attribute "etcd"
 default[:etcd][:seed_node] = "etcd01"
 default[:etcd][:search_cook] = "ktc-etcd"
 
-# setup to source from github build
-default[:etcd][:url]     = "https://dl.dropboxusercontent.com/u/848501/etcd-130907.tar.gz"
-default[:etcd][:sha256]  = "6867fc7b6a53d60d93f2c53c3a06d4f5d8143ad06ca26a76b8d5b4d830248c2d"
-default[:etcd][:version] = "130907"
-
 # retry for many times.. helps for fresh cluster building
 default[:etcd][:args] = "-c #{ipaddress}:4001 -s #{ipaddress}:7001 -r 100"
 
