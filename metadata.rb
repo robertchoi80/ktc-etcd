796c54029b94d382f818ea091c06b237d374da81
Jesse Nelson <spheromak@gmail.com>
2014-02-06 15:34:09 -0800

    bump to 2.0 version of etcd cook

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
-name    "ktc-etcd"
+name    'ktc-etcd'
 version '1.1.14'
 
-depends "etcd", ">= 1.3.4"
-depends "partial_search", ">= 1.0.2"
-depends "ktc-monitor"
+depends 'etcd', '>= 2.0.0'
+depends 'partial_search', '>= 1.0.2'
+depends 'ktc-monitor'
