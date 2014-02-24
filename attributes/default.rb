f555351fbd57d09c7504ebc62ebae0bda3988e7a
Jesse Nelson <spheromak@gmail.com>
2014-02-23 20:09:10 -0800

    use our repo package

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,19 @@
 include_attribute 'etcd'
+include_attribute 'ktc-package'
+
+return if node[:ha_disabled] == true
+# use binary from our repo
+default[:etcd][:version] = '0.3.0'
+default[:etcd][:sha256] = '18be476ba59db42c573ee23fbe00f4a205830ac54f752c0d46280707603c9192'
+default[:etcd][:url] = "http://#{node[:repo_host]}/prod/storage/etcd-0.3.0.tar.gz"
+
+
+default[:etcd][:search_cook] = 'ktc-etcd'
+
+default[:etcd][:addr] = ipaddress
+default[:etcd][:peer_addr] = ipaddress
+
+default[:etcd][:args] = "-peer-bind-addr=#{ipaddress} -bind-addr=#{ipaddress} -cors='*'"
 
 # can override in env
 default[:etcd][:seed_node] = 'etcd01'
