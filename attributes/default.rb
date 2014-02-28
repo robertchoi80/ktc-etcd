5603a3c9a859c3df551598bdc46d122bb2ee8ff0
Robert <taeil.choi@kt.com>
2014-02-28 10:26:41 +0900

    make it always override addr & peer-addr

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,20 +6,18 @@ default[:etcd][:version] = '0.3.0'
 default[:etcd][:sha256] = '18be476ba59db42c573ee23fbe00f4a205830ac54f752c0d46280707603c9192'
 default[:etcd][:url] = "http://#{node[:repo_host]}/prod/kt/storage/etcd-0.3.0.tgz"
 
-return if node[:ha_disabled] == true
-
-default[:etcd][:search_cook] = 'ktc-etcd'
-
+# In vagrant, these attrs should be overriden to use management IP.
 default[:etcd][:addr] = ipaddress
 default[:etcd][:peer_addr] = ipaddress
-
 default[:etcd][:args] = "-peer-bind-addr=#{ipaddress} -bind-addr=#{ipaddress} -cors='*'"
-
-# can override in env
-default[:etcd][:seed_node] = 'etcd01'
-default[:etcd][:search_cook] = 'ktc-etcd'
 
 # process monitoring
 default[:etcd][:processes] = [
   { 'name' =>  'etcd', 'shortname' =>  'etcd' }
 ]
+
+return if node[:ha_disabled] == true
+
+# can override in env
+default[:etcd][:seed_node] = 'etcd01'
+default[:etcd][:search_cook] = 'ktc-etcd'
